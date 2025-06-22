f41b7242b3755dc9d1fc9f564f2ed79d91483760
Moldovan <moldovan@gmail.com>
2025-06-22 08:41:23 +0200

    feat: add periodic refresh to iframe content and CSP headers

--- app/page.js
+++ app/page.js
@@ -1,4 +1,18 @@
+"use client";
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  useEffect(() => {
+    // Refresh every 30 seconds
+    const interval = setInterval(() => {
+      setRefreshKey(key => key + 1);
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div style={{
       width: '100vw',
@@ -12,6 +26,7 @@ export default function Home() {
         height: '1080px',
       }}>
       <iframe
+        key={refreshKey}
         src="https://api.togetherforvictory.com/backend/backend/slideshow?event-id=1779&slideshow-id=2202&date-from=1748728800000&enable-poster=0&token=eyJhbGciOiJIUzI1NiJ9.eyJzbGlkZXNob3dJZCI6MjIwMn0.iLS_ebzWrDt665-bUo2FhGN7dNGer2gSvUSfo7Uwfqc"
         style={{
           width: '100%',
